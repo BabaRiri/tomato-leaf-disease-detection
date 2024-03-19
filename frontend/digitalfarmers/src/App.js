0bc4a65844e3aee67640ae11c1a45b3da960e004
unknown <sewardchard@gmail.com>
2024-03-19 23:11:57 +0200

    created a react app and install material ui library, icons librray and the fonts libray

---
+++ frontend/digitalfarmers/src/App.js
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material';
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h1">Hello World</Typography>
+    </div>
+  );
+}
+
+export default App;
